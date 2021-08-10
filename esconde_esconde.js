0473b47f415a36d8d020caa46651f14e545305c9
moabebatista <moabebatista@gmail.com>
2021-08-10 18:29:59 -0300

    add questao esconde esconde

---
+++ esconde_esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+    //seu codigo aqui
+  if(numero <= 20) {
+    console.log("VOCE CONSEGUE, MANINHO")
+  }
+  else {
+    for(let i = 21; i <= numero; i++) {
+        console.log(i)
+    }
+    
+  }
+}
